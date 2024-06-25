aee8cb082e40e85e35551c2b97ecd13053cf6fee
betalantz <betalantz@gmail.com>
2024-06-25 11:16:24 -0700

    corrections to models and config

--- 02-REST-API-Flask-pt1/server/models.py
+++ 02-REST-API-Flask-pt1/server/models.py
@@ -1,12 +1,13 @@
-
 from flask_sqlalchemy import SQLAlchemy
+
 # 6. ✅ Import `SerializerMixin` from `sqlalchemy_serializer`
 
 db = SQLAlchemy()
 
+
 # 7. ✅ Pass `SerializerMixin` to `Productions`
 class Production(db.Model):
-    __tablename__ = 'productions'
+    __tablename__ = "productions"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
@@ -19,18 +20,19 @@ class Production(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    crew_members = db.relationship('CrewMember', backref='production')
+    cast_members = db.relationship("CrewMember", back_populates="production")
 
     # 7.1 ✅ Create a serialize rule that will help add our `crew_members` to the response and remove created_at and updated_at.
-        #7.2 Demo serialize_only by only allowing title to be included in the response
-        #    once done remove or comment the serialize_only line. 
+    # 7.2 Demo serialize_only by only allowing title to be included in the response
+    #    once done remove or comment the serialize_only line.
 
     def __repr__(self):
-        return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
+        return f"<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>"
+
 
 # 8. ✅ Pass `SerializerMixin` to `CrewMember`
-class CrewMember(db.Model):
-    __tablename__ = 'crew_members'
+class CastMember(db.Model):
+    __tablename__ = "cast_members"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -38,11 +40,13 @@ class CrewMember(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
-    
-    # 8.1 ✅ Create a serialize rule that will help add our `production` to the response.
-      
-    def __repr__(self):
-        return f'<Production Name:{self.name}, Role:{self.role}'
+    production_id = db.Column(db.Integer, db.ForeignKey("productions.id"))
+    production = db.relationship("Production", back_populates="cast_members")
 
- # 9. ✅ Navigate back to `app.py` for further steps.
+    # 8.1 ✅ Create a serialize rule that will help add our `production` to the response.
+
+    def __repr__(self):
+        return f"<Production Name:{self.name}, Role:{self.role}"
+
+
+# 9. ✅ Navigate back to `app.py` for further steps.
